5d46547f5402f1b360a8acdf674459cc56d66c81
IrfanRao1 <saimbro98@gmail.com>
2024-04-08 00:20:45 +0500

    second commit

---
+++ 3_Name_Case/name.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "iran Rao";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.replace(/\b\w/g, (Char) => Char.toUpperCase()));
---
+++ 3_Name_Case/name.ts
@@ -0,0 +1,10 @@
+
+
+
+let personName  = "iran Rao";
+
+console.log(personName.toLowerCase());
+
+console.log(personName.toUpperCase());
+
+console.log(personName.replace(/\b\w/g, (Char) => Char.toUpperCase()));
