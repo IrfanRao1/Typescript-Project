e1b8c73a753d7723fbbbe59291e369ad82a7f852
IrfanRao1 <saimbro98@gmail.com>
2024-04-12 23:44:03 +0500

    commit

---
+++ 18_Seeing _The_World/seeing_the_world.js
@@ -0,0 +1,21 @@
+"use strict";
+// Store the locations in a array.
+let places = ['Itlay', 'Germany', 'Agra', 'Eifel Tower', 'Times square'];
+// print your array in itsogiginal order.
+console.log('orignal' + places);
+// Print your array in alphabetical
+console.log('copy' + [...places].sort());
+// print your array in reverse alphabetical
+console.log('orignal' + places);
+// // print your array in reverse alphabetical in its orignal order
+console.log('copy' + [...places].sort().reverse());
+// Show that array is still in orignal
+console.log('orignal' + places);
+// Reverse the order of your list
+console.log('orignal' + places.reverse());
+// reverse the order of your list again
+console.log('orignal' + places.reverse());
+// Sort your array to its stored in alphabetical
+console.log('orignal' + places.sort());
+// sort to change your array so it is stored alphabetical order
+console.log('orignal' + places.reverse());
