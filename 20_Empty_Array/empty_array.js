9942a6234bbe7569f71e78ab26c5f86beb989511
IrfanRao1 <saimbro98@gmail.com>
2024-04-12 23:53:13 +0500

    commit

---
+++ 20_Empty_Array/empty_array.js
@@ -0,0 +1,9 @@
+"use strict";
+// thing of something you could store in a array.
+let items = ['pakistan ', 'karachi', 'sindh'];
+// write a program the creates a list containingthese item.
+let item2 = [];
+item2.push('Germany');
+item2.push('England');
+item2.push('Pakistan');
+console.log(item2);
---
+++ 20_Empty_Array/empty_array.ts
@@ -0,0 +1,12 @@
+
+// thing of something you could store in a array.
+let items :string[] = ['pakistan ','karachi','sindh'];
+
+// write a program the creates a list containingthese item.
+let item2 :string[] = [] ;
+
+item2.push('Germany');
+item2.push('England');
+item2.push('Pakistan');
+
+console.log(item2);
