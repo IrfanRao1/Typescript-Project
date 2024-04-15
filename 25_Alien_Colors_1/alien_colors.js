ed89ad8db571b2db7d508c6a9c1c5649e7c57171
IrfanRao1 <saimbro98@gmail.com>
2024-04-15 21:55:55 +0500

    commit

---
+++ 25_Alien_Colors_1/alien_colors.js
@@ -0,0 +1,14 @@
+"use strict";
+// Create a variable called alien_color
+let alien_color = 'green';
+// write an if statement to test whether the alien'scolor is green. if it is, 
+// print a message that the player just earned 5 point
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points !");
+}
+// Write one version of this program that passes the if test and another that fails.
+alien_color = 'yellow';
+// The version that fails will have no output.
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points !");
+}
