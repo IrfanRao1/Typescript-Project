2a982407b6bc7f86cba380f8d2a3a17ed1047037
IrfanRao1 <saimbro98@gmail.com>
2024-04-15 21:42:13 +0500

    commit

---
+++ 21_Typescript_Objects/typescript_object.js
@@ -0,0 +1,11 @@
+"use strict";
+//  write a program that object containing these item.
+// let peson name :string = "irfan
+// const person name array :string[] = ['person', 'car', 'cold drink',];
+let person = {
+    age: 26,
+    name: 'irfan',
+    nationality: 'pakistan',
+    student: true
+};
+console.log(person);
---
+++ 21_Typescript_Objects/typescript_object.ts
@@ -0,0 +1,19 @@
+
+//  write a program that object containing these item.
+
+
+interface person {
+    age : number,
+    name : string,
+   nationality :string,
+    student : boolean
+}
+
+let person :person = {
+age : 26 ,
+name : 'irfan' ,
+nationality : 'pakistan' ,
+student : true
+}
+
+console.log(person);
