fb8d3fdfd7e4e35c87eeabdfd3ff7f8e540c6670
IrfanRao1 <saimbro98@gmail.com>
2024-04-15 22:03:20 +0500

    commit

---
+++ 29_Favorite_Fruit/favorite_fruit.ts
@@ -0,0 +1,25 @@
+// Favorite fruit: Make  a array of your fvourit fruit, and then write a series
+// independent if statement  that check for cartain  inyour array
+
+let favourite_fruite: string[] = ['apple', 'banana', 'mango',];
+
+if(favourite_fruite.includes('apple')){
+    console.log('I really like apple');
+}
+
+if(favourite_fruite.includes('banana')){
+    console.log('I really like banana');
+}
+
+if(favourite_fruite.includes('mango')){
+    console.log('I really like mango');
+}
+
+if(favourite_fruite.includes('strawberry')){
+    console.log('I really like strawberry');
+}
+
+
+if(favourite_fruite.includes('orange')){
+    console.log('I really like orange');
+}
