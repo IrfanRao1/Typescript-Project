cb87bbb57894216bed017eda25a3231134ce6e79
IrfanRao1 <saimbro98@gmail.com>
2024-04-15 21:52:12 +0500

    commit

---
+++ 23_Conditional_tests/conditional_tests.js
@@ -0,0 +1,13 @@
+"use strict";
+// write a series of conditional tests.
+// creating car veriable
+let car = 'subaru';
+// test no 1
+console.log("is car === 'subaru'? I perdict True.");
+console.log(car === 'subaru');
+// test no 2
+console.log("is car == 'subaru'? I perdict True.");
+console.log(car == 'subaru');
+// test no 3
+console.log("is car != 'Honda'? I perdict True.");
+console.log(car != 'Honda');
---
+++ 23_Conditional_tests/conditional_tests.ts
@@ -0,0 +1,45 @@
+
+// write a series of conditional tests.
+
+// creating car veriable
+let car :string = 'subaru' ;
+
+// test no 1
+console.log("is car === 'subaru'? I perdict True.");
+console.log(car ==='subaru');
+
+// test no 2
+console.log("is car == 'subaru'? I perdict True.");
+console.log(car =='subaru');
+
+// test no 3
+console.log("is car != 'Honda'? I perdict True.");
+console.log(car != 'Honda');
+
+// test no 4
+console.log("is car !== 'ford'? I perdict True.");
+console.log(car !== 'ford');
+
+// test no 5
+console.log("is car.Upper case == 'SUBARU'? I perdict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+
+// test no 6
+console.log("is car == 'SUBARU'? I perdict False.");
+console.log(car == 'SUBARU');
+
+// test no 7
+console.log("is car === 'SUBARU'? I perdict False.");
+console.log(car === 'SUBARU');
+
+// test no 8
+console.log("is car === 'Train'? I perdict False.");
+console.log(car === 'Train');
+
+// test no 9
+console.log("is car === 'Bus'? I perdict False.");
+console.log(car === 'Bus');
+
+// test no 10
+console.log("is car === 'Cycle'? I perdict False.");
+console.log(car === 'Cycle');
