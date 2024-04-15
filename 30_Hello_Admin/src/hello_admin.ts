132fab375a04d698a77def264e50436308116fca
IrfanRao1 <saimbro98@gmail.com>
2024-04-15 22:04:34 +0500

    commit

---
+++ 30_Hello_Admin/src/hello_admin.ts
@@ -0,0 +1,21 @@
+
+
+//   Print a generic greting, such  a hello Eric, thank you  for logging in again
+
+const userNames: string[] = ['admin','Bugs Bunny','Daffy DUCK', 'Sylvester','Tweety'];
+
+userNames.forEach(userNames => {
+
+    if(userNames === 'admin')
+{
+    console.log('Hellow admin, would you like to use a status report?');
+}
+
+else
+
+{
+
+    console.log(`Hello ${userNames}, thank you for logging in again.`)
+}
+
+})
