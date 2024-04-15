095c2d4c2bf30d82f150e1efeda2d65f9ed462e9
IrfanRao1 <saimbro98@gmail.com>
2024-04-15 21:49:12 +0500

    commit

---
+++ 22_International_Error/international_error.ts
@@ -0,0 +1,12 @@
+
+// If you have not an array index error in one of your programs yet, try 
+// to make one happen.
+// creating array
+
+let newArray :string[] = ['will smith', 'james bond', 'tom cruies'];
+
+// accesing array by index
+console.log(newArray[5]);
+
+// correct
+console.log(newArray);
