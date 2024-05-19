8f391b9ab5567cdb8c62ea0fa021ce191ab0129c
IrfanRao1 <saimbro98@gmail.com>
2024-05-19 12:49:19 +0500

    commit

---
+++ 36_T_Shirt/dist/t_shirt.js
@@ -0,0 +1,9 @@
+"use strict";
+// T_Shirt: Write a function called make_shirt() that accepts a size and text of a message
+// call the function.
+function make_shirt(size, text) {
+    console.log(`creatinga ${size} shirt with the message: ${text}`);
+}
+make_shirt('large', 'Hello world');
+make_shirt('medium', 'Hello world');
+make_shirt('small', 'Hello world');
---
+++ 36_T_Shirt/src/t_shirt.ts
@@ -0,0 +1,13 @@
+// T_Shirt: Write a function called make_shirt() that accepts a size and text of a message
+
+// call the function.
+
+function make_shirt(size:string, text:string){
+
+    console.log(`creatinga ${size} shirt with the message: ${text}`);
+}
+make_shirt('large','Hello world');
+
+make_shirt('medium','Hello world');
+
+make_shirt('small','Hello world');
