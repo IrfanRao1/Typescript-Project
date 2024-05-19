5a86f0471c36db74df0b5e4925cbeee83bbb4daf
IrfanRao1 <saimbro98@gmail.com>
2024-05-19 13:02:00 +0500

    commit

---
+++ 39_City_Names/src/city_names.ts
@@ -0,0 +1,17 @@
+
+// City name: write a function called city_country() that takes in the name of a city
+
+// Call your functiom with at least tree city_country paries.
+
+function city_country(city: string, country: string){
+   
+    return `${city},${country}`;
+}
+
+console.log(city_country('karachi','Pakistan'));
+
+
+console.log(city_country('tokyo','japan'));
+
+
+console.log(city_country('paries','france'));
