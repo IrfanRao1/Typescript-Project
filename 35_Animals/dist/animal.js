ea4b12dea900d53cfb37210f7087d6bf97bfc0ee
IrfanRao1 <saimbro98@gmail.com>
2024-05-19 12:45:17 +0500

    commit

---
+++ 35_Animals/dist/animal.js
@@ -0,0 +1,7 @@
+"use strict";
+// Animals: Think of at least three different animals that have a common characteristic.
+// cloud print a sentence such as any these anmals would make a great pet!
+let Animals = ['dog', 'cat', 'rabbit',];
+for (let Animal of Animals) {
+    console.log(`A ${Animal} would make a greate pet!`);
+}
---
+++ 35_Animals/src/animal.ts
@@ -0,0 +1,9 @@
+// Animals: Think of at least three different animals that have a common characteristic.
+// cloud print a sentence such as any these anmals would make a great pet!
+
+let Animals: string[] = ['dog','cat','rabbit',];
+
+for(let Animal of Animals){
+
+console.log(`A ${Animal} would make a greate pet!`);
+}
