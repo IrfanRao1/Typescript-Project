4f04910f5fea03af261afa6eaa916cb869e916b1
IrfanRao1 <saimbro98@gmail.com>
2024-06-08 01:17:44 +0500

    commit

---
+++ 40_Album/dist/album.js
@@ -0,0 +1,18 @@
+"use strict";
+// Album: write a function called make_album () that builds a object describe a music album.
+function make_album(artist, title, tracks) {
+    const album = {
+        artist: artist,
+        title: title
+    };
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
+}
+const album1 = make_album('Artist 1', 'Album Title 1');
+console.log(album1);
+const album2 = make_album('Artist 2', 'Album Title 2');
+console.log(album2);
+const album3 = make_album('Artist 3', 'Album Title 3', 12);
+console.log(album3);
